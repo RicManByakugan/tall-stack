f16b76107ce0ae2582de967a6efb6daf82edcd70
Ratovonirina <eric.ratovonirina@gmail.com>
2025-05-31 11:11:53 +0300

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    @livewireStyles
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body>
+    <livewire:some-component />
+    @livewireScripts
+</body>
+</html>
